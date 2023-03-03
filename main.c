c8c6d39083e650999a37b18d60f35645ba126b78
Stefan <stefan.holmberg@systementor.se>
2023-03-03 10:32:21 +0100

    aaa

--- main.c
+++ main.c
@@ -23,8 +23,71 @@
 // #define true 100
 
 
+void Replace(){
+    int arrayen[] = {44,12,345,2,545,45,66,2};
+    int antal2 = sizeof(arrayen);
+    printf("%d\n",antal2);
+    int antal3 = sizeof(arrayen[0]);
+    printf("%d\n",antal3);
+    int antal = sizeof(arrayen) / sizeof(arrayen[0]);
+    for(int i = 0; i < antal;i++){
+        if(arrayen[i] % 2 == 0)
+            arrayen[i] = 0;
+    }
+    for(int i = 0; i < antal; i++){
+        printf("%d\n",arrayen[i]);
+    }
+
+}
+
+//  customers[]- dynamisk addnewcustomer
+// dynamiska datastrukturer - HEJ DÅ TILL STACKEN
+// HEAP
+
+void gym2(){
+    printf("Hur många domare var det?");
+    int antal;
+    scanf(" %d",&antal);
+    float gymnasticResultat[antal]; // DEN ÄR ALLTID antal
+    // 5 domare
+    for(int i=0; i<antal;i++ ){
+        printf("Mata in domarreultat:%d", i+1);
+        scanf(" %f", &gymnasticResultat[i]);
+        // float f;
+        // scanf(" %f", &f);
+        // gymnasticResultat[i] = f;
+    }
+        
+}
+
+
+#define NR_OF_REFEREES 15
 int main(){
+    gym2();
+    Replace();
+    //List<Player> 
     
+    //char namn[100];
+    float gymnasticResultat[NR_OF_REFEREES];
+    // 5 domare
+    for(int i=0; i<NR_OF_REFEREES;i++ ){
+        printf("Mata in domarreultat:%d", i+1);
+        scanf(" %f", &gymnasticResultat[i]);
+        // float f;
+        // scanf(" %f", &f);
+        // gymnasticResultat[i] = f;
+    }
+    // "algoritm" högsta
+    float largestSoFar  = gymnasticResultat[0];
+    float smallestSoFar  = gymnasticResultat[0];
+    for(int i = 1; i < NR_OF_REFEREES; i++){
+        if(gymnasticResultat[i] > largestSoFar)
+            largestSoFar = gymnasticResultat[i];
+        if(gymnasticResultat[i] < smallestSoFar)
+            smallestSoFar = gymnasticResultat[i];
+    }
+    printf("Största blev %f", largestSoFar);
+
     
     int b = oldIndicator(40);
     if(b == YOUNG){
